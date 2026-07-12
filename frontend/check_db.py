bc9baac4342de7168e406ecebeebed5f26d49949
Scifially <sahaj.mauryaa@gmail.com>
2026-07-12 21:11:15 +0530

    chore: remove useless scratch and test files

--- frontend/check_db.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://postgres:postgres@127.0.0.1:5433/upstox_bot')
-cur = conn.cursor()
-cur.execute("SELECT ts, open, high, low, close, volume FROM candles WHERE symbol='BHARTIARTL'")
-rows = cur.fetchall()
-for r in rows:
-    if r[2] > 3000:
-        print('Bad candle:', r)
